fcca82a350108c39a7e2d15c5919f1facf467a87
tracy7894 <113955729+tracy7894@users.noreply.github.com>
2024-04-08 00:21:59 +0800

    Update two-sum.java

--- two-sum.java
+++ two-sum.java
@@ -1,13 +1,15 @@
-class twoSum {
+class Solution {
     public int[] twoSum(int[] nums, int target) {
         int sum;
         int[] result=new int[2];
         for(int i=0;i<nums.length;i++){
-            sum=nums[i]+nums[i+1];
+            for(int j=i+1;j<nums.length;j++){
+            sum=nums[i]+nums[j];
             if(sum==target){
                 result[0]=i;
-                result[1]=i+1;
+                result[1]=j;
                 break;
+            }
             } 
         }
             return result;
