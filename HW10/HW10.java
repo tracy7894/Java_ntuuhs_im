e5680bef20b169ee31c5e6d95d8ecbfce38afb21
tracy7894 <113955729+tracy7894@users.noreply.github.com>
2024-05-07 20:55:05 +0800

    Update HW10.java

--- HW10/HW10.java
+++ HW10/HW10.java
@@ -1,68 +1,31 @@
 import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.ObjectOutput;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
+
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 class HW10{
     public static void main(String[] args) throws IOException {
-        //String filename="HW10.csv";
-        //FileOutputStream f1=new FileOutputStream(filename);
-        File filename=new File("HW10.csv");
-        
         Map<String,Integer> school=new HashMap<>();
-        String student="0",line;
+        String student="0";
         int a=0;
         Scanner s=new Scanner(System.in);
         int menu=0;
-        FileReader fileReader=new FileReader(filename);
-        BufferedReader bufferedReader=new BufferedReader(fileReader);
-        if(filename.exists()) {
-            try{
-            
-                while((line=bufferedReader.readLine())!=null){
-                    String[] words=new String[2];
-                    // words=line.split(",");
-                    // if (words.length >= 2) { // 检查数组长度是否足够
-                    //     String name = words[0];
-                    //     int grade = Integer.parseInt(words[1]);
-                    //     school.put(name, grade);
-                    // } else {
-                    //     // 如果数据格式不符合预期，可以选择跳过或者记录错误
-                    //     System.out.println("Invalid data format: " + line);
-                    // }
-                   String name=words[0];
-                   int grade=Integer.parseInt(words[1]);
-                    school.put(name, grade);
-                }
-    }
-
-    catch(IOException e){
-        e.printStackTrace();;
-    }
+        File filecheak = new File("grades.csv");
+        if (filecheak.exists()) {
+            loadFormFile(school,"grades.csv");
         }
-        else{
-            try {filename.createNewFile();}
-            catch(IOException e){
+        else
+            try {
+                filecheak.createNewFile();
+            } catch (IOException e){
                 e.printStackTrace();
-        }
-        }
-
-        
+            }
         while(menu!=6){
             System.out.println("輸入功能[1]新增成績,[2]查詢成績,[3]修改學生成績,[4]刪除學生成績,[5]列出所有學生成績[6]退出:");
             menu=s.nextInt();
@@ -120,24 +83,30 @@ class HW10{
         
     }
     //
-     FileWriter fileWriter=new FileWriter(filename);
-        
-        BufferedWriter bufferedWriter=new BufferedWriter(fileWriter);
-    try{
-       
-         for (Map.Entry<String, Integer> entry : school.entrySet()) {
-               String contain=(entry.getKey() + ", " + entry.getValue());
-               bufferedWriter.newLine();
-              bufferedWriter.write(contain);
-    }}
-    catch(IOException e){
-        e.printStackTrace();
-    }
-    bufferedWriter.close();
-     
+    saveToFile(school, "grades.csv");
     }
 
-    private static Integer valueOf(String string) {
-        return null;
+static void loadFormFile(Map<String, Integer> grades,String filename) {
+    try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] parts = line.split(",");
+            String name = parts[0];
+            int grade = Integer.parseInt(parts[1]);
+            grades.put(name, grade);
+        }
+    } catch (IOException e) {
+        System.out.println("讀取檔案時發生錯誤:"+e.getMessage());
     }
 }
+
+static void saveToFile(Map<String, Integer> grades,String filename) {
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+        for (Map.Entry<String, Integer> entry : grades.entrySet()) {
+            writer.write(entry.getKey() + "," + entry.getValue());
+            writer.newLine();
+        }
+    } catch (IOException e) {
+        System.out.println("寫入檔案時發生錯誤:" + e.getMessage());
+    }
+}}
